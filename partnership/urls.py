764834b40e4f90e70cfb9bd4ba4c9a76618a8b2b
Leesoouyeon <112999029+Leesoouyeon@users.noreply.github.com>
2022-12-16 17:03:26 +0900

    Add files via upload

--- partnership/urls.py
+++ partnership/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.partnership),
+    path('', views.cafe),
+    path('home', views.cafe, name='cafe'),
+    path('convenience', views.convenience, name='convenience'),
+    path('food', views.food, name='food'),
+    path('bar', views.bar, name='bar'),
+    path('index_cafe', views.index_cafe),
+    path('index_convenience', views.index_convenience),
 ]
--- partnership/views.py
+++ partnership/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 
 # Create your views here.
-def partnership(request):
+def cafe(request):
     return render(
         request,
         'partnership/home.html',
+    )
+
+def convenience(request):
+    return render(
+        request,
+        'partnership/Convenience.html',
+    )
+def food(request):
+    return render(
+        request,
+        'partnership/food.html',
+    )
+
+def bar(request):
+    return render(
+        request,
+        'partnership/bar.html',
+    )
+
+def index_cafe(request):
+    return render(
+        request,
+        'location/index_cafe.html',
+    )
+
+def index_convenience(request):
+    return render(
+        request,
+        'location/index_convenience.html',
     )
